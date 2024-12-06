8d11346ed403397be76893f170ef67a83cb60a65
Remco Vaes <remco.vaes@brightcubes.nl>
2024-12-06 07:55:05 +0100

    solve day 6

--- day6/main.go
+++ day6/main.go
@@ -5,24 +5,150 @@ import (
 	"strings"
 )
 
+type coordinate struct {
+	x int
+	y int
+}
+
 func TotalResultA(str string) int {
-	return 0
+	linesRaw := splitIntoNewlines(str)
+	lines := make([][]string, 0)
+	for _, line := range linesRaw {
+		lines = append(lines, strings.Split(line, ""))
+	}
+
+	start, direction := FindStart(lines)
+	visited := make(map[coordinate]bool)
+	current := start
+	currentDirection := direction
+	for {
+		visited[current] = true
+		newPos, newDir, done := GetNextPos(lines, current, currentDirection)
+		if done {
+			return len(visited)
+		}
+		current = newPos
+		currentDirection = newDir
+	}
 }
 
 func TotalResultB(str string) int {
-	return 0
+	linesRaw := splitIntoNewlines(str)
+	lines := make([][]string, 0)
+	for _, line := range linesRaw {
+		lines = append(lines, strings.Split(line, ""))
+	}
+
+	start, direction := FindStart(lines)
+	visited := make(map[coordinate]bool)
+	current := start
+	currentDirection := direction
+	for {
+		visited[current] = true
+		newPos, newDir, done := GetNextPos(lines, current, currentDirection)
+		if done {
+			break
+		}
+		current = newPos
+		currentDirection = newDir
+	}
+	current = start
+	total := 0
+	for coord := range visited {
+		hasLoop := HasLoop(lines, coord, start, direction)
+		if hasLoop {
+			total++
+		}
+	}
+
+	return total
 }
 
-func Helper1(str string) bool {
+func FindStart(str [][]string) (coordinate, string) {
+	for indexY, xline := range str {
+		for indexX, current := range xline {
+			if current == "^" {
+				return coordinate{indexX, indexY}, "up"
+			}
+			if current == ">" {
+				return coordinate{indexX, indexY}, "right"
+			}
+			if current == "<" {
+				return coordinate{indexX, indexY}, "left"
+			}
+			if current == "v" {
+				return coordinate{indexX, indexY}, "down"
+			}
+		}
+	}
+	panic("no guard found")
+}
+
+func GetNextPos(lines [][]string, current coordinate, direction string) (coordinate, string, bool) {
+	if direction == "up" {
+		current.y--
+	}
+	if direction == "down" {
+		current.y++
+	}
+	if current.y < 0 || current.y >= len(lines) {
+		return current, "done", true
+	}
+	if direction == "right" {
+		current.x++
+	}
+	if direction == "left" {
+		current.x--
+	}
+
+	if current.x < 0 || current.x >= len(lines[0]) {
+		return current, "done", true
+	}
+
+	if current.y < 0 || current.y >= len(lines[0]) {
+		return current, "done", true
+	}
+	if lines[current.y][current.x] == "#" {
+		if direction == "up" {
+			current.y++
+			x := coordinate{current.x, current.y}
+			return x, "right", false
+		}
+		if direction == "down" {
+			current.y--
+			return coordinate{current.x, current.y}, "left", false
+		}
+		if direction == "right" {
+			current.x--
+			return coordinate{current.x, current.y}, "down", false
+		}
+		if direction == "left" {
+			current.x++
+			return coordinate{current.x, current.y}, "up", false
+		}
+	}
+	return coordinate{current.x, current.y}, direction, false
+}
+
+func HasLoop(lines [][]string, coordToCheck coordinate, start coordinate, direction string) bool {
+	current := start
+	currentDirection := direction
+	lines[coordToCheck.y][coordToCheck.x] = "#"
+	for i := 0; true; i++ {
+		if i > 10000 {
+			lines[coordToCheck.y][coordToCheck.x] = "."
+			return true
+		}
+		newPos, newDir, done := GetNextPos(lines, current, currentDirection)
+		current = newPos
+		currentDirection = newDir
+		if done {
+			break
+		}
+	}
+	lines[coordToCheck.y][coordToCheck.x] = "."
+
 	return false
-}
-
-func Helper2(str string) int {
-	return 0
-}
-
-func Helper3(str string) int {
-	return 0
 }
 
 func splitIntoNewlines(str string) []string {
@@ -30,7 +156,7 @@ func splitIntoNewlines(str string) []string {
 }
 
 func main() {
-	string := getDataDay()
+	string := GetDataDay()
 	println("Result part 1: ", TotalResultA(string))
 	println("Result part 2: ", TotalResultB(string))
 }
@@ -41,7 +167,7 @@ func check(e error) {
 	}
 }
 
-func getDataDay() string {
+func GetDataDay() string {
 	dat, err := os.ReadFile("input")
 	check(err)
 	return string(dat)
--- day6/main_test.go
+++ day6/main_test.go
@@ -7,32 +7,32 @@ import (
 )
 
 func TestTotalA(t *testing.T) {
-	input := ``
+	input := `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
 	result := TotalResultA(input)
-	assert.Equal(t, 143, result)
+	assert.Equal(t, 41, result)
 }
 
 func TestTotalB(t *testing.T) {
-	input := ``
+	input := `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
 
 	result := TotalResultB(input)
-	assert.Equal(t, 123, result)
-}
-
-func TestHelper1(t *testing.T) {
-	input := ``
-	result := Helper1(input)
-	assert.Equal(t, result, 6)
-}
-
-func TestHelper2(t *testing.T) {
-	input := ``
-	result := Helper2(input)
-	assert.Equal(t, result, 6)
-}
-
-func TestHelper3(t *testing.T) {
-	input := ``
-	result := Helper3(input)
-	assert.Equal(t, result, 6)
+	assert.Equal(t, 6, result)
 }
