e8da7b8b6de9567bebdde346bf68a33e15d014ff
Remco Vaes <remco.vaes@brightcubes.nl>
2024-12-09 23:29:49 +0100

    setup python

---
+++ day10python/main.py
@@ -0,0 +1,56 @@
+
+from typing import List
+
+
+def read_file():
+    with open("input") as f:
+        return f.read()
+
+class SingleInput:
+    single: str
+
+class MainInput:
+    lines: List[SingleInput] = []
+
+def parse_single(single: str) -> SingleInput:
+    single_input = SingleInput()
+
+    single_input.single = single
+
+    return single_input
+
+
+def parse_input(s: str) -> MainInput:
+    main = MainInput()
+    for line in s.split("\n"):
+        main.lines.append(parse_single(line))
+    return main
+
+def process_single1(single: SingleInput) -> int:
+    return 0
+
+
+def process_single2(single: SingleInput) -> int:
+    return 0
+
+def solve_total_1():
+    file = read_file()
+    return solver_part1(file)
+
+def solve_total_2():
+    file = read_file()
+    return solver_part2(file)
+
+def solver_part1(s: str):
+    input = parse_input(s)
+    total = 0
+    for i in input.lines:
+        total+=process_single1(i)
+    return 0
+
+def solver_part2(s: str):
+    input = parse_input(s)
+    total = 0
+    for i in input.lines:
+        total+=process_single2(i)
+    return 0
