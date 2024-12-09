70979216d3931cb1c51341c4b3e5ca94a30c4d10
Remco Vaes <remco.vaes@brightcubes.nl>
2024-12-09 13:07:16 +0100

    temp

--- day9/main.go
+++ day9/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
 type SingleInput struct {
-	single string
+	id        int
+	position  int
+	free      int
+	size      int
+	filled    int
+	filleable bool
 }
 
 type PuzzleInput struct {
@@ -16,13 +22,24 @@ type PuzzleInput struct {
 func parseInput(str string) PuzzleInput {
 	sanitized := strings.Trim(str, "\n\t ")
 	input := PuzzleInput{}
-	for _, item := range strings.Split(sanitized, "\n") {
-		if item == "" {
-			continue
+	position := 0
+	strlist := strings.Split(sanitized, "")
+	for index := 0; index < (len(strlist)); index += 2 {
+		fileSize, _ := strconv.Atoi(strlist[index])
+		freeSpaceCurrent := 0
+		if len(strlist) > index+1 {
+			freeSpaceCurrent, _ = strconv.Atoi(strlist[index+1])
 		}
-		singleInput := SingleInput{single: item}
+		singleInput := SingleInput{}
+		singleInput.id = index / 2
+		singleInput.position = position
+		singleInput.free = freeSpaceCurrent
+		singleInput.size = fileSize
+		singleInput.filleable = true
 
 		input.lines = append(input.lines, singleInput)
+
+		position = position + fileSize + freeSpaceCurrent
 	}
 	return input
 }
@@ -41,9 +58,46 @@ func TotalResultB(str string) int {
 
 func HelperPart1(input PuzzleInput) int {
 	total := 0
-	for _, line := range input.lines {
-		total = HelperSinglePart1(line)
+
+	reversedIndex := len(input.lines) - 1
+
+	lines := input.lines
+	for index := range len(lines) {
+		current := make([]int, 0)
+		for range input.lines[index].size {
+			current = append(current, input.lines[index].id)
+		}
+		input.lines[index].size = 0
+
+		for input.lines[index].free != 0 && reversedIndex >= index {
+			if input.lines[index].position > input.lines[reversedIndex].position {
+				break
+			}
+			if input.lines[reversedIndex].size == 0 {
+				reversedIndex--
+				continue
+			}
+			for range input.lines[reversedIndex].size {
+				current = append(current, input.lines[reversedIndex].id)
+				input.lines[reversedIndex].size--
+				input.lines[index].free--
+
+				if input.lines[index].free == 0 {
+					break
+				}
+			}
+		}
+		if input.lines[index].free < 0 {
+			panic("can not happen")
+		}
+		if input.lines[index].size < 0 {
+			panic("can not happen")
+		}
+		for i, final := range current {
+			total = total + (input.lines[index].position+i)*final
+		}
 	}
+
 	return total
 }
 
@@ -53,8 +107,66 @@ func HelperSinglePart1(line SingleInput) int {
 
 func HelperPart2(input PuzzleInput) int {
 	total := 0
-	for _, line := range input.lines {
-		total = HelperSinglePart1(line)
+
+	lines := input.lines
+	for index := range len(lines) {
+
+		reversedIndex := len(input.lines) - 1
+		nextIndex := 0
+		for {
+			if nextIndex > reversedIndex {
+				break
+			}
+			next := input.lines[nextIndex]
+			last := input.lines[reversedIndex]
+			if last.size == 0 {
+				reversedIndex--
+				continue
+			}
+
+			if nextIndex >= reversedIndex {
+				input.lines[reversedIndex].filleable = false
+				reversedIndex--
+				continue
+			}
+			if input.lines[reversedIndex].size == 0 || !last.filleable {
+				reversedIndex--
+				continue
+			}
+			if next.position > last.position {
+				break
+			}
+			if input.lines[reversedIndex].size > next.free {
+				nextIndex++
+				continue
+			}
+
+			for i := range input.lines[reversedIndex].size {
+				id := input.lines[reversedIndex].id
+				pos := i + next.position + next.size + next.filled
+				total += id * pos
+			}
+
+			input.lines[nextIndex].free = next.free - last.size
+			input.lines[nextIndex].filled += last.size
+
+			input.lines[reversedIndex].size = 0
+		}
+		if input.lines[index].free < 0 {
+			panic("can not happen")
+		}
+		if input.lines[index].size < 0 {
+			panic("can not happen")
+		}
+	}
+
+	for index := range len(lines) {
+		currentLine := input.lines[index]
+		for i := range currentLine.size {
+			xid := input.lines[index].id
+			post := input.lines[index].position + i
+			total += xid * post
+		}
 	}
 	return total
 }
@@ -77,6 +189,8 @@ func check(e error) {
 
 func GetDataDay() string {
 	dat, err := os.ReadFile("input")
+	l := len(dat)
+	print(l)
 	check(err)
 	return string(dat)
 }
