869ab53560a46d49076256766d44ddfea374c850
Remco Vaes <remco.vaes@brightcubes.nl>
2024-12-16 20:38:51 +0100

    fix day 16

--- day15python/main.py
+++ day15python/main.py
@@ -1,6 +1,7 @@
 from typing import List, Tuple
 
 Coor = Tuple[int, int]
+Box = Tuple[int, int, str]
 
 def up(s: Coor):
     return (s[0]-1, s[1])
@@ -10,6 +11,17 @@ def left(s: Coor):
     return (s[0], s[1]-1)
 def right(s: Coor):
     return (s[0], s[1]+1)
+
+def upb(s: Box):
+    return (s[0]-1, s[1], s[2])
+def downb(s: Box):
+    return (s[0]+1, s[1], s[2])
+def leftb(s: Box):
+    return (s[0], s[1]-1, s[2])
+def rightb(s: Box):
+    return (s[0], s[1]+1, s[2])
+
+
 
 
 
@@ -23,7 +35,7 @@ class SingleInput:
 class MainInput:
     lines: List[SingleInput]
     robot_pos: Coor
-    boxes: set[Coor]
+    boxes: set[Box]
     walls: set[Coor]
 
 def parse_single(single: str) -> SingleInput:
@@ -44,11 +56,13 @@ def parse_input(s: str) -> MainInput:
         items = item
         for j, s in enumerate(items):
             if s == "#":
-                main.walls.add((i,j))
+                main.walls.add((i,j *2))
+                main.walls.add((i ,j * 2 + 1))
             if s == "O":
-                main.boxes.add((i,j))
+                main.boxes.add((i,j * 2, "A"))
+                main.boxes.add((i,j * 2 + 1, "B"))
             if s == "@":
-                main.robot_pos = (i,j)
+                main.robot_pos = (i,j * 2)
 
     s = splitted[1].replace("\n", "")
     for n in s:
@@ -67,29 +81,73 @@ def move_robot(c: Coor, s: str):
         return down(c)
     raise Exception("invalid move")
 
+def move_box(c: Box, s: str):
+    if s == ">":
+        return rightb(c)
+    if s == "<":
+        return leftb(c)
+    if s == "^":
+        return upb(c)
+    if s == "v":
+        return downb(c)
+    raise Exception("invalid move")
+
+
 def process_single1(main: MainInput, single: SingleInput):
     move = single.single
     next_pos = move_robot(main.robot_pos, move)
     if next_pos in main.walls:
         return
-    if not next_pos in main.boxes:
+    a_box = (*next_pos, "A")
+    b_box = (*next_pos, "B")
+    if not a_box in main.boxes and not b_box in main.boxes:
         main.robot_pos = next_pos
         return
 
-    boxes_to_move = []
-    boxes_moved = []
-    next_box = next_pos
+    boxes_to_move = set()
+    boxes_to_move = set()
+
+    boxes_moved = set()
+
+    robot_pos = main.robot_pos
     while True:
-        boxes_moved.append(next_box)
-        next_box = move_robot(next_box, move)
-        boxes_to_move.append(next_box)
-        if next_box in main.walls:
-            return
-        if next_box not in main.boxes:
+        robot_pos = move_robot(robot_pos, move)
+        start = len(boxes_to_move)
+        boxes_to_check = set()
+        for b in boxes_to_move:
+            boxes_to_check.add(move_robot((b[0], b[1]), move))
+
+        if len(boxes_to_check) == 0:
+            boxes_to_check.add(robot_pos)
+        for box_to_check in boxes_to_check:
+            a_box = (*box_to_check, "A")
+            b_box = (*box_to_check, "B")
+            if (a_box in main.boxes) and move != "<":
+                hit_a = a_box
+                hit_b = (hit_a[0], hit_a[1] + 1, "B")
+                boxes_to_move.add(hit_a)
+                boxes_to_move.add(hit_b)
+                boxes_moved.add(move_box(hit_a, move))
+                boxes_moved.add(move_box(hit_b, move))
+            if (b_box in main.boxes) and move != ">":
+                hit_b = b_box
+                hit_a = (hit_b[0], hit_b[1] - 1, "A")
+                boxes_to_move.add(hit_a)
+                boxes_to_move.add(hit_b)
+                boxes_moved.add(move_box(hit_a, move))
+                boxes_moved.add(move_box(hit_b, move))
+        if start == len(boxes_to_move):
             break
+        
     for b in boxes_moved:
-        main.boxes.remove(b)
+        wall = (b[0], b[1])
+        if wall in main.walls:
+            return
+
     for b in boxes_to_move:
+        if b in main.boxes:
+            main.boxes.remove(b)
+    for b in boxes_moved:
         main.boxes.add(b)
     main.robot_pos = next_pos
 
@@ -114,7 +172,8 @@ def solver_part1(s: str):
     for i in input.lines:
         process_single1(input, i)
     for box in input.boxes:
-        total = total + box[0] * 100 + box[1]
+        if box[2] == "A":
+            total = total + box[0] * 100 + box[1]
     return total
 
 def solver_part2(s: str):
@@ -123,3 +182,6 @@ def solver_part2(s: str):
     for i in input.lines:
         total+=process_single2(i)
     return total
+
+if __name__ == "__main__":
+    print(solve_total_1())
--- day15python/main_test.py
+++ day15python/main_test.py
@@ -3,10 +3,25 @@ from main import solve_total_1, solve_total_2, solver_part1, solver_part2
 def test_total_1():
     result = solve_total_1()
     assert result != 1350859
-    assert result == 1294459
+    assert result != 1316311
+    assert result == 0
 
 def test_example_1():
-    input = """##########
+    input = """#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^
+"""
+    result = solver_part1(input)
+    assert result == 10092
+
+def test_total_2():
+    input ="""##########
 #..O..O.O#
 #......O.#
 #.OO..O.O#
@@ -25,16 +40,10 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
 >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
 <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
-^^>vv<^v^v<vv>^<><v<^
-v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
-"""
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
     result = solver_part1(input)
-    assert result == 10092
-
-def test_total_2():
-    result = solve_total_2()
-    assert result == 0
+    assert result == 9021
 
 def test_example_2():
     input = """
