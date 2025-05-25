4e03d5258a536feda39d2b04c3267802a810e660
Remco Vaes <remcovaes@protonmail.com>
2025-05-25 18:36:50 +0200

    solve day 8 part 1

---
+++ 2015/day8/advent/main.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+import re
+
+@dataclass
+class SingleInput:
+    raw: str
+
+@dataclass
+class FullInput:
+    single: list[SingleInput]
+
+def parse_line(string: str) -> SingleInput:
+    return SingleInput(raw=string)
+
+def parse_input_full(string: str) -> FullInput:
+    full = FullInput(single=[])
+    for line in string.split("\n"):
+        full.single.append(parse_line(line))
+    return full
+
+def read_input() -> FullInput:
+    with open("advent/input", encoding="utf-8") as file:
+        return parse_input_full(file.read())
+
+
+def solve_part_one(full_input: FullInput):
+    total = 0
+    for i in full_input.single:
+        total += solve_part_one_single(i)
+    return total
+
+def solve_part_one_single(single: SingleInput):
+    total_len = len(single.raw)
+    string_stripped = single.raw.strip("\"")
+    without_quote = string_stripped.replace(r'\"', "a")
+    without_slash = without_quote.replace(r'\\', "a")
+    without_hex = re.sub(r'\\x..', "a", without_slash)
+
+    return total_len - len(without_hex)
+
+def solve_part_two(full_input: FullInput):
+    total = 0
+    for i in full_input.single:
+        total += solve_part_two_single(i)
+    return total
+
+def solve_part_two_single(single: SingleInput):
+    total = len(single.raw)
+    return total
+
+def solve():
+    lines = read_input()
+    part_a = solve_part_one(lines)
+    print("Part 1: ", part_a)
+
+    part_b = solve_part_two(lines)
+    print("Part 2: ", part_b)
+
+if __name__ == "__main__":
+    solve()
---
+++ 2015/day8/tests/test_main.py
@@ -0,0 +1,35 @@
+import pytest
+
+from advent.main import parse_line, read_input, solve_part_one, solve_part_one_single, solve_part_two, solve_part_two_single
+
+
+@pytest.mark.parametrize(
+    [ "input_string", "expected" ],
+    [
+        [ "\"\"", 2 ],
+        [ "\"abc\"", 2 ],
+        [ r'"aaa\"aaa"', 3 ],
+        [ r'"a\"aa\"aa"', 4 ],
+        [ r'"a\\aa\"aa"', 4 ],
+        [ r'"\x11"', 5 ],
+    ]
+)
+def test_part_one_single(input_string, expected):
+    present = parse_line(input_string)
+    assert solve_part_one_single(present) == expected
+
+def test_part1_total():
+    assert solve_part_one(read_input()) == 0
+
+@pytest.mark.parametrize(
+    [ "input_string", "expected" ],
+    [
+        [ "abc", 3 ],
+    ]
+)
+def test_part2(input_string, expected):
+    present = parse_line(input_string)
+    assert solve_part_two_single(present) == expected
+
+def test_part2_total():
+    assert solve_part_two(read_input()) == 0
